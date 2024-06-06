fed6b10a9a41506de7696f8a5a71b71cae61895a
zak <imzakbro@gmail.com>
2024-06-06 04:48:42 +0100

    nrm

--- NeuralNetwork/funcs/activation.py
+++ NeuralNetwork/funcs/activation.py
@@ -1,12 +1,28 @@
 from math import exp
 
+NRM = True  # normalize
+
+
+def normalize(x):
+    while x > 1:
+        x /= 10
+    return x
+
 
 def sigmoid(x):
     return 1 / (1 + exp(-x))
 
 
-def relu( x):
-        return max(0, x)
+def relu(x):
+    x = max(0, x)
+    if NRM:
+        x = normalize(x)
+
+    return x
+
 
 def linear(x):
+    if NRM:
+        x = normalize(x)
+
     return x
