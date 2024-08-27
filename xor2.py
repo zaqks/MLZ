ab2232b4f846f3c0811518c96a35f15bf7702e9d
zak <imzakbro@gmail.com>
2024-08-27 23:18:03 +0100

    json

--- xor2.py
+++ xor2.py
@@ -1,7 +1,7 @@
 from lib import *
 
 
-ntwrk = Network([2, 3, 1], [TanH(), TanH(), TanH()], Mse())
+ntwrk = Network([2, 3,   1], [TanH(), TanH(), TanH()], Mse())
 
 
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
@@ -10,7 +10,7 @@ Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
 
 ntwrk.import_params()
 
-ntwrk.train(X, Y)
-ntwrk.export_params()
+#ntwrk.train(X, Y, epochs=20000)
+#ntwrk.export_params()
 
 ntwrk.run(X)
