98750e4323ae17fb64483c7813ba0e153189060a
zak <imzakbro@gmail.com>
2024-08-29 05:09:10 +0100

    conv

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .lib.networks.network import *
 from .lib.layers.dense_layer import Dense
+from .lib.layers.convolutional_layer import Convolutional
 from .lib.layers.activations.activation_layer import *
 from .lib.losses.loss import *
