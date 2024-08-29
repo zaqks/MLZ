f7a78946d60cfb1c54ed75300045cf8bdafcea70
zak <imzakbro@gmail.com>
2024-08-29 05:12:29 +0100

    cnn

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from .lib.networks.network import *
+
 from .lib.layers.dense_layer import Dense
 from .lib.layers.convolutional_layer import Convolutional
+from .lib.layers.reshape_layer import Reshape
+
 from .lib.layers.activations.activation_layer import *
 from .lib.losses.loss import *
