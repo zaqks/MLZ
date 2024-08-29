b11295b03b2e7b81d5386c4bbac8f9ce7b8e29cd
zak <imzakbro@gmail.com>
2024-08-29 04:37:08 +0100

    lib

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .lib.networks.network import *
+from .lib.layers.dense_layer import Dense
+from .lib.layers.activations.activation_layer import *
+from .lib.losses.loss import *
--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from .networks.network import *
-from .layers.dense_layer import Dense
-from .layers.activations.activation_layer import *
-from .losses.loss import *
